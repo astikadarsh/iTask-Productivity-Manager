3d4c1f565b4353fcdfdde376b9151ae997fd6cd5
astikadarsh <astikadarsh7061@gmail.com>
2025-12-25 21:12:06 +0530

    third commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,17 +4,36 @@ import Navbar from './components/Navbar'
 
 function App() {
 
+  const handleEdit=()=>{
+    
+  }
+
+  const handleDelete=()=>{
+
+  }
+
+  const handleAdd=()=>{
+
+  }
+
+
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100">
-        <div className="addTodo">
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+        <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a To Do</h2>
+          <input type="text" className='w-1/2' />
+          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>Add</button>
         </div>
         <h2 className='text-lg font-bold'>Your To Dos</h2>
         <div className="todos">
-          <div className="todo">
-
+          <div className="todo flex">
+            <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+            <div className="buttons">
+              <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
+              <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
+            </div>
           </div>
         </div>
       </div>
